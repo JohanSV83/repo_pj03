9c03dca439b883115adfbb5a85d09890b9698254
Johan Sanchez <you@example.com>
2024-07-26 18:45:55 -0500

    agregue una funcion de mostrar correlacion

---
+++ exploracionDx.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def Mostrar_Correlacion(df):
+  correlation_matrix = df[['Administration','R&D Spend','Marketing Spend','Profit']].corr()
+  print(correlation_matrix)
+  sns.heatmap(correlation_matrix, annot=True, cmap='Blues')
+  return  plt.show()
