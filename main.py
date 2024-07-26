a84371714a2f70b595846faa94f3db96a5f86464
Johan Sanchez <you@example.com>
2024-07-26 18:54:21 -0500

    se hizo merge de la rama explorador y importe su funcion a archivo main

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import exploracionDx.py  as ex
+
+
+
+data=pd.read_csv('https://raw.githubusercontent.com/krishnaik06/Multiple-Linear-Regression/master/50_Startups.csv')
+
+
