d3628153ede32555d56d1754f895ebb1b60f8098
Jhania Pedraza <jhania28@gmail.com>
2024-07-26 22:00:30 -0500

    correccion 2

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import exploracionDx.py  as ex
-import tratamientoDx.py as tr
+from exploracionDx.py  as ex
+from tratamientoDx.py as tr
 
 
 data=pd.read_csv('https://raw.githubusercontent.com/krishnaik06/Multiple-Linear-Regression/master/50_Startups.csv')
