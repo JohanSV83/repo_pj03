757f8e142625adf53799f2f5a955bf112e39cf00
Johan Sanchez <you@example.com>
2024-07-26 21:23:25 -0500

    Se ha agregado en main.py las invocaciones de las funciones mostrar correlacion eliminar duplicados mostrar pairplot

--- exploracionDx.py
+++ exploracionDx.py
@@ -12,3 +12,16 @@ def Mostrar_Correlacion(df):
 
   def Mostrar_Grafico_Pairplot(df):
     return sns.pairplot(df)
+  
+
+  def eliminar_filas_erroneas(df,eliminar_nulos=True,eliminar_duplicados=True,condicion=None):
+    if eliminar_nulos:
+      df.dropna()
+    if eliminar_duplicados:
+      df.drop_duplicates()
+    if condicion is not None:
+      df=condicion(df)
+
+    return df  
+
+
