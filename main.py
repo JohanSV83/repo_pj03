956df7fa3f82b4aae9fb554c4517438d0a8c14cb
Johan Sanchez <you@example.com>
2024-07-26 22:08:08 -0500

    correcion #3

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from exploracionDx.py  as ex
-from tratamientoDx.py as tr
+import exploracionDx.py  as explo
+import tratamientoDx.py as tr
 
 
 data=pd.read_csv('https://raw.githubusercontent.com/krishnaik06/Multiple-Linear-Regression/master/50_Startups.csv')
@@ -11,13 +11,13 @@ data=pd.read_csv('https://raw.githubusercontent.com/krishnaik06/Multiple-Linear-
 opcion=input("ingresa opcion 1 para ver correlacion de variables\ningresa opcion 2 para eliminar columnas erroneas\ningresa opcion 3 para ver pairplot\ningresa opcion 4 limpiar filas duplicadas")
 def menu():
     if opcion=='1':
-      ex.Mostrar_Correlacion(data)
+      explo.Mostrar_Correlacion(data)
     elif opcion=='2':
       tr.eliminar_filas_erroneas(data)
     elif opcion=='3':
       tr.Eliminar_Limpiar_datos(data)
     elif opcion=='4':
-      ex.Mostrar_Grafico_Pairplot(data) 
+      explo.Mostrar_Grafico_Pairplot(data) 
 
 
 menu()
