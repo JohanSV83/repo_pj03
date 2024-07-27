6b2677b28c7b754467fb6e0e6fc82ed04b2c8977
Jhania Pedraza <jhania28@gmail.com>
2024-07-26 19:02:55 -0500

    he agregado la funcion eliminar nulos y importe la funcion a mamain .py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import exploracionDx.py  as ex
-
+import tratamientoDx.py as tr
 
 
 data=pd.read_csv('https://raw.githubusercontent.com/krishnaik06/Multiple-Linear-Regression/master/50_Startups.csv')
